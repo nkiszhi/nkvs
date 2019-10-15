131a6839dbd1f8dbb2e09c30f8b4ec571143d2bd
Wang Zhi <wztgex@gmail.com>
2019-10-15 14:50:38 +0800

    update vsuz.py

--- vsuz.py
+++ vsuz.py
@@ -10,17 +10,35 @@ DIR_ZIP = "/home/RaidDisk/virusshare/"
 # password for zip files
 PASSWORD = "infected"
 
-def extract():
+# unzip virusshare zip files in DIR_ZIP to specified folder DIR_DATA
+def vs_unzip():
     z = os.listdir(DIR_ZIP)
     for f in z:
-        print DIR_ZIP+f
-        os.popen("unzip -o -P " + PASSWORD + " " + DIR_ZIP+f + " -d " + DIR_DATA)
+        path = DIR_ZIP+f
+        os.popen("unzip -o -P " + PASSWORD + " " + path + " -d " + DIR_DATA)
+        os.popen("rm -f " + path)
     pass
 
-def get_sha256():
+# calculate sample sha256 and mv sample to specified folder
+def vs_sha256():
+    files = os.listdir(DIR_DATA)
+    for i in files:
+        path = DIR_DATA + i
+        print path
+        if os.path.isdir(path):
+            continue
+        
+        sha256 = os.popen("sha256sum " + path)
+        sha256 = sha256.read().split(' ')[0]
+        #print sha256
+        dst_path = DIR_DATA + sha256[0] + "/" +sha256[1] + "/" +sha256[2] + "/" +sha256[3] + "/" + sha256
+        os.popen("mv " + path + " " + dst_path)
+        print dst_path
+
     pass
 
 def main():
-    extract()
+    vs_unzip()
+    vs_sha256()
 
 main()

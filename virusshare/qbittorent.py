992a9edb9f4b3d4ded3c86849e7bde947d82603b
chengkai1121 <36880308+chengkai1121@users.noreply.github.com>
2019-11-26 19:30:16 +0800

    Delete qbittorent.py

--- virusshare/qbittorent.py
+++
@@ -1,9 +0,0 @@
-from qbittorrent import Client
-
-qb = Client('http://127.0.0.1:8080/')
-qb.login()
-list = qb.preferences()
-print(list)
-torrents = qb.torrents()
-for torrent in torrents:
-    print(torrent['name'])
